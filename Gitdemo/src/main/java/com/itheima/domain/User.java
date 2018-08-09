44570af981b345ccb649077d4f7a892455cd4842
zzq <zzq@qq.com>
2018-08-09 21:57:51 +0800

    tianjia

--- Gitdemo/src/main/java/com/itheima/domain/User.java
+++ Gitdemo/src/main/java/com/itheima/domain/User.java
@@ -3,6 +3,17 @@ package com.itheima.domain;
 public class User {
 	
 	private String username;
+	
+	private String password;
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getUsername() {
 		return username;
